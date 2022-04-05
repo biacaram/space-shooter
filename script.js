9950ddf12fa1726e45d49cca711f817c6c382dfa
Beatriz Andre <biacaram@gmail.com>
2022-04-05 00:00:17 -0300

    laser

--- script.js
+++ script.js
@@ -39,4 +39,34 @@ function moveDown() {
     }
 }
 
-window.addEventListener('keydown', flyShip)
+// tiro
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = './img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
+
+function moveLaser(laser){
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+
+        if(xPosition === 380) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`
+        }
+    }, 10);
+}
+
+window.addEventListener('keydown', flyShip);
